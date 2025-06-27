6e054c856821d49184566eb030ea4f10c9b260b8
BassamMustafa16 <basam_wadi@yahoo.com>
2025-06-28 00:27:43 +0300

    Test Vercel and Render Connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["https://higalo-bassammustafa16s-projects.vercel.app/"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello World");
---
+++ frontend/src/app/test/page.tsx
@@ -0,0 +1,17 @@
+import api from "@/lib/api";
+export default function Test() {
+  const fetchData = async () => {
+    try {
+      const res = await api.get("/");
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={fetchData}>Fetch Data</button>
+    </div>
+  );
+}
