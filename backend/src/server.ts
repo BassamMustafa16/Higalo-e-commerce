b9cb862bd62401378e368e2a46ea9630080aea99
BassamMustafa16 <basam_wadi@yahoo.com>
2025-06-28 00:00:27 +0300

    Try to set Render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello World");
   return;
 });
