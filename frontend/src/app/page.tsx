b342b8c6324bd904dd9b87e5ff65c988b0d08b4e
BassamMustafa16 <basam_wadi@yahoo.com>
2025-06-27 23:08:57 +0300

    test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-8">
       {/* Hero Section */}
-      <Hero />
+      {/* <Hero /> */}
       {/* Categories  */}
       <Categories />
       {/* Best Sellers of the Month  */}
