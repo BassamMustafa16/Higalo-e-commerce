ba2295163625ea8a3aa61b08d7e162a8d6c16172
BassamMustafa16 <basam_wadi@yahoo.com>
2025-06-30 15:48:34 +0300

    Issue Solved

--- frontend/src/app/account/page.tsx
+++ frontend/src/app/account/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function AccountPage() {
   const router = useRouter();
-  const userName = localStorage.getItem("firstName");
+  const [userName, setUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    setUserName(localStorage.getItem("firstName"));
+  }, []);
+
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("firstName");
     router.push("/");
   };
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex flex-col gap-5 items-center py-5">
-      <h1>Hello - {userName}</h1>
+      <h1>Hello - {userName ?? ""}</h1>
       <button
         className="bg-darkBlue text-white py-2 px-4 font-semibold rounded-xl"
         onClick={handleLogout}
